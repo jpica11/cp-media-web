fe50c2abf41f6dd29bfa14acef000313a093137e
James Pica <jamespica11@gmail.com>
2018-08-10 20:54:47 -0400

    Initial commit for site

---
+++ cp-media-backend/src/main/java/com/cp/media/spring/CpMediaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.cp.media.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpMediaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpMediaBackendApplication.class, args);
+	}
+}
